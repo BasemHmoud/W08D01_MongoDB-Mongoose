fc7a76be154d57fc4cf338fc7672aee0c37da28d
C05_Basem <basemhmoud735@gmail.com>
2021-05-16 23:45:02 +0300

    finish All puls check

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const express=require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
-const port =5000
+const port = 5000;
+app.use(express.json());
 
-const db=require("./db")
+const db = require("./db");
 
+const todoModul = require("./schema");
 
 app.listen(port, () => {
-    console.log(`server run on port ${port}`);
-  });
-  
+  console.log(`server run on port ${port}`);
+});
--- schema.js
+++ schema.js
@@ -1,10 +1,10 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const todoSchema=new mongoose({
-   task:{type :string} ,
-   description:{type :string} ,
-   deadline:{type :Date} ,
-   isCompleted:{type :Boolean} ,
-   priority:{type :string} 
-})
-module.exports=mongoose.model("Todo",todoSchema)
+const todoSchema = new mongoose({
+  task: { type: string },
+  description: { type: string },
+  deadline: { type: Date },
+  isCompleted: { type: Boolean },
+  priority: { type: string },
+});
+module.exports = mongoose.model("Todo", todoSchema);
